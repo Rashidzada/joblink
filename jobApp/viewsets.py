e5a3e16cecbab9e99de959e62fa826b4530d9334
Rashidzada <rashidzada6@gmail.com>
2023-11-30 07:54:39 +0500

    add changes to the api book permision

--- jobApp/viewsets.py
+++ jobApp/viewsets.py
@@ -1,5 +1,5 @@
 from .serializers import*
-from rest_framework import viewsets
+from rest_framework import viewsets,permissions
 
 class AuthorViewSet(viewsets.ModelViewSet):
     queryset = Author.objects.all()
@@ -8,8 +8,10 @@ class AuthorViewSet(viewsets.ModelViewSet):
 class UserViewSet(viewsets.ReadOnlyModelViewSet):
     queryset = User.objects.all()
     serializer_class = UserSerializer
+    permission_classes = [permissions.AllowAny]
 
 
 class BookViewSet(viewsets.ModelViewSet):
     queryset = Book.objects.all()
     serializer_class = BookSerializer
+    permission_classes = [permissions.IsAuthenticatedOrReadOnly]
